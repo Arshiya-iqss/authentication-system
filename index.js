b2680d337a31488eded75b700ab963785752ddfe
Arshiya Shakeel <arshiya@dc.iquasar.com>
2023-11-15 14:15:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const connectDB = require("./db");
+const bodyparser = require("body-parser");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// MongoDB connection
+connectDB();
+
+// Parsing JSON request body
+// app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+// Defining authentication routes
+app.use("/auth", authRoutes);
+
+// Defining user routes
+app.use("/user", userRoutes);
+
+// Starting server
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
