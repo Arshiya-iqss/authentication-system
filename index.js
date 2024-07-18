2e59568d14458750e6165f33ed1e8c5ca07bb0c8
Arshiya-iqss <143513190+Arshiya-iqss@users.noreply.github.com>
2024-07-18 18:23:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 // Parsing JSON request body
-// app.use(express.json());
+app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
